a787d3d10bc55b15b36539848b4646235dd7c208
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-06-09 20:25:46 +0300

    Tasks Cleaned

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
-import Header from './Components/Header';
-import Form from "./Components/Form";
-import Task from "./Components/Task";
 import { useState } from "react";
-import { getAllTasks, getNumberOfTasks, removeTaskFromStorage } from "./Storage";
+import Form from "./Components/Form";
+import Header from './Components/Header';
+import AllTasks from './Components/AllTasks';
+import { getAllTasks, getNumberOfTasks } from "./Storage";
 
 export default function App() {
 
   let [tasks, setTasks] = useState(getAllTasks());
 
-  function deleteTask(id) {
-    removeTaskFromStorage(id, setTasks)
-  }
-
   return (
-    <>
+    <div style={{ minHeight: '101vh' }}>
+
       <Header />
 
-      <div className='container my-5' style={{ minHeight: '85vh' }}>
+      <div className='container my-5'>
 
         <h3>لديك {getNumberOfTasks()} مهام اليوم</h3>
 
-        <div className='shadow-sm my-4 bg-white rounded'>
-          {tasks.length ?
-            tasks.map(task => <Task key={task.id} task={task} deleteTask={deleteTask} />)
-            :
-            <h3 className='text-center py-5'>ليس لديك مهام اليوم</h3>
-          }
-        </div>
+        <AllTasks tasks={tasks} setTasks={setTasks} />
 
         <Form setTasks={setTasks} />
 
       </div>
-    </>
+      
+    </div>
   )
 }
--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,12 +1,12 @@
 import { useEffect, useRef, useState } from "react"
-import { updateTaskInStorage } from "../Storage";
+import { removeTaskFromStorage, updateTaskInStorage } from "../Storage";
 import Fade from "./Fade"
 
 let uncheckedStyle = 'fa-regular fa-circle fa-xl icon-hover'
 let checkedStyle = 'fa-solid fa-circle-check fa-xl icon-hover'
 let hoverStyle = 'fa-regular fa-circle-check fa-xl icon-hover'
 
-export default function Task({ task, deleteTask }) {
+export default function Task({ task, setTasks }) {
 
   let [checked, setChecked] = useState(task.checked);
   let [iconStyle, setIconStyle] = useState('')
@@ -60,8 +60,8 @@ export default function Task({ task, deleteTask }) {
 
         <i
           onClick={() => {
-            // we can simply deleteTask(id) but let's add animation
-            setTimeout(deleteTask, 250, task.id);
+            // we can simply removeTaskFromStorage(task.id, setTasks) but let's add animation
+            setTimeout(removeTaskFromStorage, 250, task.id, setTasks);
             card.current.style.animation = 'fade-out 350ms'
             card.current.style.opacity = '0';
           }}
