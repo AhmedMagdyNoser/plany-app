c03d3da8ab378f9a53bf4cd2ecc6675de7ad1d3a
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-06-24 12:03:55 +0300

    Initial Home Page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
   return (
     <div className="container my-5">
-      Home Page
+      <h2>الصفحة الرئيسية</h2>
+      <nav className="d-flex gap-3">
+          <Link to={'/tasks'} className="text-decoration-none">قائمة المهام</Link>
+          <Link to={'/notes'} className="text-decoration-none">ملاحظات</Link>
+        </nav>
     </div>
   )
 }
