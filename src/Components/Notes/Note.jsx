7561f4c62d1e57d96a3e200bfe9380d569c0b6cb
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-06-25 13:35:18 +0300

    Get Notes to Notes Page

--- src/Components/Notes/Note.jsx
+++ src/Components/Notes/Note.jsx
@@ -1,12 +1,14 @@
-export default function Note() {
+export default function Note({ note }) {
 
   return (
     <>
-      <div className="bg-white px-4 py-3 rounded shadow-sm position-relative">
-        <h4>العنوان</h4>
-        <p className="text-muted">نحن من نصنع الحياة بأفعالنا وأفكارنا وتصرفاتنا، لذلك علينا العمل على تحقيق أكبر قدر من السعادة والإنجازات والتأثير الإيجابي في العالم من حولنا.</p>
+      <div className="bg-white px-4 py-3 rounded shadow-sm d-flex flex-column justify-content-between">
+        <div>
+          <h4>{note.title}</h4>
+          <p className="text-muted">{note.description}</p>
+        </div>
         <div className="text-muted d-flex justify-content-between align-items-center border-top pt-3">
-          <span>June 24, 2023</span>
+          <span>{note.date}</span>
           <span className="d-flex gap-2">
             <i className="fa-solid fa-edit cursor-pointer option"></i>
             <i className="fa-solid fa-trash cursor-pointer option"></i>
--- src/Pages/NotesPage.jsx
+++ src/Pages/NotesPage.jsx
@@ -1,17 +1,14 @@
 import Note from "../Components/Notes/Note"
 import NewNote from "../Components/Notes/NewNote";
+import { useSelector } from "react-redux";
 
 export default function NotesPage() {
 
+  const notes = useSelector(store => store.notes.data)
+
   return (
-    <div className='container my-4 list'>
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
+    <div className='container list my-4'>
+      {notes.length ? notes.map(note => <Note key={note.id} note={note} />) : null }
       <NewNote />
     </div>
   )
