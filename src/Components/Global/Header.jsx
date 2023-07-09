9844ed232d850705025852ff5b1c1c902316ef6a
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-07-09 13:21:07 +0300

    NavMenu Created

--- src/Components/Global/Header.jsx
+++ src/Components/Global/Header.jsx
@@ -1,17 +1,28 @@
 import { Link } from "react-router-dom";
+import NavMenu from "./NavMenu";
+import { useState } from "react";
 
 export default function Header() {
+
+  const [isNavMenuOpened, setIsNavMenuOpened] = useState(false);
+
   return (
-    <header className="bg-dark py-3">
-      <div className="container d-flex justify-content-between align-items-center">
-        <div className="fw-bold fs-2">
-          <Link to={'/'} className="text-white text-decoration-none">اللوجو</Link>
+    <>
+      <header className="bg-dark py-3">
+        <div className="container d-flex justify-content-between align-items-center">
+          <div className="fw-bold fs-2">
+            <Link to={"/"} className="text-white text-decoration-none">
+              <img
+                style={{ height: "50px" }}
+                src={require("../../Imgs/plany.ico")}
+                alt="Plany"
+              />
+            </Link>
+          </div>
+          <i className="fa-solid fa-bars text-white fa-2x cursor-pointer" onClick={() => setIsNavMenuOpened(true)} ></i>
         </div>
-        <nav className="d-flex gap-3">
-          <Link to={'/tasks'} className="text-white text-decoration-none">قائمة المهام</Link>
-          <Link to={'/notes'} className="text-white text-decoration-none">ملاحظات</Link>
-        </nav>
-      </div>
-    </header>
-  )
+      </header>
+      <NavMenu isOpened={isNavMenuOpened} setIsOpened={setIsNavMenuOpened}/>
+    </>
+  );
 }
---
+++ src/Components/Global/NavMenu.jsx
@@ -0,0 +1,78 @@
+import { useNavigate } from "react-router-dom";
+
+export default function NavMenu({ isOpened, setIsOpened }) {
+  return (
+    <div
+      className={`nav-menu bg-white shadow-lg ${
+        !isOpened && "nav-menu-closed"
+      }`}
+    >
+      <div
+        style={{ background: "#f5f5f5" }}
+        className="border-bottom p-3 d-flex justify-content-between align-items-center"
+      >
+        <span className="fw-bold">الاقسام</span>
+        <i
+          onClick={() => setIsOpened(false)}
+          className="fa-solid fa-xmark p-2 rounded cursor-pointer"
+        ></i>
+      </div>
+      <div className="d-flex flex-column">
+        <NavLink onClick={() => setIsOpened(false)} title="قائمة المهام" path="/tasks" iconClass='fa-solid fa-circle-check' />
+        <NavLink onClick={() => setIsOpened(false)} title="الملاحظات" path="/notes" iconClass='fa-solid fa-pen-clip' />
+      </div>
+
+      <style>
+        {`
+          .nav-menu {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 300px;
+            max-width: 100%;
+            min-height: 100vh;
+            z-index: 9;
+            transition: 0.35s
+          }
+          .nav-menu-closed {
+            left: -300px;
+            shadow: none !important;
+          }
+          .fa-xmark {
+            transition: 0.25s;
+          }
+          .fa-xmark:hover {
+            background: #eee;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
+
+function NavLink({ title, path, iconClass, onClick  }) {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate(path);
+    onClick();
+  }
+
+  return (
+    <div onClick={handleClick} className="link p-4 cursor-pointer text-muted d-flex gap-2 align-items-center border-bottom">
+      <i className={iconClass}></i>
+      {title}
+      <style>
+        {`
+          .link {
+            transition: 0.2s;          
+          }
+          .link:hover {
+            color: black !important;
+            background: #eee;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
