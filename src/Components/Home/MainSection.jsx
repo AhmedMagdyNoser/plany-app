b7fe1218469b7b86bc3c2edb72494cd609e00032
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-07-20 09:48:37 +0300

    Home Main Section

---
+++ src/Components/Home/MainSection.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import { FadeIn } from "../Utils/Fade";
+
+export default function MainSection() {
+  return (
+    <FadeIn milliSeconds="1000">
+      <div className="container py-5 flex-center flex-column gap-4 border-bottom">
+        <h1 className="text-center text-dark py-4 border-bottom">اجعل حياتك أكثر تنظيما وفعالية</h1>
+        <p className="text-center text-muted" style={{ width: "500px", maxWidth: "90%" }}>
+          باستخدام الأدوات المناسبة لتحقيق النجاح ، قم بزيادة إنتاجيتك إلى الحد الأقصى ، حدد بسهولة أولويات المهام وقم بتعيين التذكيرات.
+        </p>
+        <nav className="flex-center flex-wrap gap-3">
+          <HomeButton title="قائمة المهام" path="/tasks" iconClass="fa-solid fa-circle-check" />
+          <HomeButton title="الملاحظات" path="/notes" iconClass="fa-solid fa-pen-clip" />
+        </nav>
+      </div>
+    </FadeIn>
+  );
+}
+
+function HomeButton({ title, path, iconClass }) {
+  return (
+    <Link
+      to={path}
+      className="flex-grow-1 border-bottom flex-center flex-column text-dark gap-4 p-5 rounded opacity-hover cursor-pointer text-decoration-none"
+      style={{ background: "#eee" }}
+    >
+      <i className={iconClass + " fs-2"}></i>
+      <span className="text-center">{title}</span>
+    </Link>
+  );
+}
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,32 +1,5 @@
-import { Link } from "react-router-dom";
-import { FadeIn } from "../Components/Utils/Fade";
+import MainSection from "../Components/Home/MainSection";
 
 export default function HomePage() {
-  return (
-    <FadeIn milliSeconds="1000">
-      <div className="container py-5 flex-center flex-column gap-4">
-        <h1 className="text-center text-dark py-4 border-bottom">اجعل حياتك أكثر تنظيما وفعالية</h1>
-        <p className="text-center text-muted" style={{ width: "500px", maxWidth: "100%" }}>
-          باستخدام الأدوات المناسبة لتحقيق النجاح ، قم بزيادة إنتاجيتك إلى الحد الأقصى ، حدد بسهولة أولويات المهام وقم بتعيين التذكيرات.
-        </p>
-        <nav className="d-flex gap-3">
-          <HomeButton title="قائمة المهام" path="/tasks" iconClass="fa-solid fa-circle-check" />
-          <HomeButton title="الملاحظات" path="/notes" iconClass="fa-solid fa-pen-clip" />
-        </nav>
-      </div>
-    </FadeIn>
-  );
-}
-
-function HomeButton({ title, path, iconClass }) {
-  return (
-    <Link
-      to={path}
-      className="border-bottom flex-center flex-column text-dark gap-4 p-5 rounded opacity-hover cursor-pointer text-decoration-none"
-      style={{ background: "#eee" }}
-    >
-      <i className={iconClass + " fs-2"}></i>
-      <span className="text-center">{title}</span>
-    </Link>
-  );
+  return <MainSection />;
 }
