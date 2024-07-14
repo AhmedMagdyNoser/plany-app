6bbea2a272c183253aa69711095c4171e1364af9
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-07-14 09:23:38 +0300

    Refactor Alert component and add new SVG icons
    - Exported SVGIconProps type from SVGIcon component for reusability
    - Renamed Alert component's colors prop to be more descriptive
    - Added new Icon prop to Alert component to allow for custom icons
    - Added new CheckedCircle and ExclamationCircle SVG icons to SVGIcon component
    - Updated Alert.Error and Alert.Success components to use new SVG icons instead of plain text icons.

--- src/components/ui/Alert.tsx
+++ src/components/ui/Alert.tsx
@@ -1,15 +1,24 @@
-export default function Alert({ message, colors = "bg-gray-100 text-gray-500" }: { message: string; colors?: string }) {
+import SVGIcon, { SVGIconProps } from "../icons/SVGIcon";
+
+type AlertProps = {
+  Icon?: React.ComponentType<SVGIconProps>;
+  message: string;
+  colors?: string;
+};
+
+export default function Alert({ Icon, message, colors = "bg-gray-100 text-gray-500" }: AlertProps) {
   return (
-    <div className={`animate-fade-in-fast flex items-center gap-3 rounded-xl px-4 py-3 text-sm ${colors}`}>
+    <div className={`flex items-center gap-2 rounded-xl px-4 py-3 ${colors}`}>
+      {Icon && <Icon size={15} />}
       <div>{message}</div>
     </div>
   );
 }
 
 Alert.Error = function Error({ message = "An error occurred" }) {
-  return <Alert message={message} colors="bg-red-100 text-red-500 dark:bg-red-950" />;
+  return <Alert message={message} colors="bg-red-100 text-red-500 dark:bg-red-950" Icon={SVGIcon.ExclamationCircle} />;
 };
 
 Alert.Success = function Success({ message = "Success" }) {
-  return <Alert message={message} colors="bg-green-100 text-green-500 dark:bg-green-950" />;
+  return <Alert message={message} colors="bg-green-100 text-green-500 dark:bg-green-950" Icon={SVGIcon.CheckedCircle} />;
 };
