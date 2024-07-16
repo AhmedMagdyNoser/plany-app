25770ab3b1165f8e488eaf9fb6112ed15fb697e9
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-07-16 09:28:55 +0300

    Use the Logout functionality from a sample profile UI

--- src/pages/inner/Profile.tsx
+++ src/pages/inner/Profile.tsx
@@ -1,5 +1,23 @@
+import useLogout from "@/hooks/useLogout";
+import useUser from "@/hooks/useUser";
+
 function Profile() {
-  return <div>Profile</div>;
+  const { user } = useUser();
+  const logout = useLogout();
+
+  if (!user) return null;
+
+  const fullName = `${user.firstName} ${user.lastName}`;
+  return (
+    <div className="flex-center container flex-col gap-8 px-4 py-10">
+      <img src={user.imgUrl} alt={fullName} className="h-40 w-40 rounded-full" />
+      <h1>{fullName}</h1>
+      <p>{user.email}</p>
+      <button className="btn-basic" onClick={logout}>
+        Logout
+      </button>
+    </div>
+  );
 }
 
 export default Profile;
