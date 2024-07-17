94b9696e87c28d1cee3386c2d02b0284c2f6c946
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-07-17 07:17:16 +0300

    Update the `apiRequest` function for better response handling

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,17 +1,29 @@
 import { globalErrorMessage } from "@/utils/constants";
 
-export async function apiRequest(url: string, options: RequestInit = {}): Promise<string> {
+export async function apiRequest(url: string, options: RequestInit = {}): Promise<any> {
   try {
+    // Prepare headers
     const defaultHeaders = { "Content-Type": "application/json" };
     const finalOptions = { ...options, headers: { ...defaultHeaders, ...options.headers } };
-    let response = await fetch(`${import.meta.env.VITE_API_URL}/${url}`, finalOptions);
+
+    // Send the request
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/${url}`, finalOptions);
+
+    // throw an error if the response isn't ok
     if (!response.ok) throw await response.text();
-    return await response.text();
+
+    // return the response as string or object if it's ok
+    const contentType = response.headers.get("Content-Type");
+    const isJson = contentType && contentType.includes("application/json");
+    return isJson ? await response.json() : await response.text();
   } catch (error) {
     console.log("%cError from `apiRequest` helper function", "color: red; font-weight: bold;", error);
-    if (error instanceof TypeError && error.message === "Failed to fetch")
+    if (error instanceof TypeError && error.message === "Failed to fetch") {
       throw "Oops! We’re having trouble reaching our servers.";
-    if (typeof error === "string") throw error;
-    throw globalErrorMessage;
+    } else if (typeof error === "string") {
+      throw error;
+    } else {
+      throw globalErrorMessage;
+    }
   }
 }
