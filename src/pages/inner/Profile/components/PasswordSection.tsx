e0d9ae18be8fca667c1ba847fe5a9d7081a59b13
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-07-18 11:34:30 +0300

    Create the PasswordSection UI for the profile page

---
+++ src/pages/inner/Profile/components/PasswordSection.tsx
@@ -0,0 +1,10 @@
+function PasswordSection() {
+  return (
+    <section className="flex-center flex-col gap-3">
+      <p>Password</p>
+      <button className="btn-basic">Change Password</button>
+    </section>
+  );
+}
+
+export default PasswordSection;
