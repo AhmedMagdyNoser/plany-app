693a4fed4c325b9a716f7d8bf22557fa0463750b
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-07-18 09:56:49 +0300

    Create getCoverColor helper function

--- src/components/global/DefaultProfileImg/index.tsx
+++ src/components/global/DefaultProfileImg/index.tsx
@@ -1,11 +1,12 @@
 import useUser from "@/hooks/useUser";
+import { getCoverColor } from "@/utils/helpers";
 
 function DefaultProfileImg({ fontSize = 1 }: { fontSize?: number }) {
   const { user } = useUser();
 
   return (
     <div
-      style={{ backgroundImage: "radial-gradient(circle, #60a5fa, #3b82f6)", fontSize: `${fontSize}rem` }}
+      style={{ backgroundImage: getCoverColor("orange"), fontSize: `${fontSize}rem` }}
       className="flex-center h-full w-full rounded-full text-white"
     >
       {user ? user.firstName[0].toUpperCase() : "NA"}
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -13,6 +13,22 @@ export function getUserFromAccessToken(accessToken: string) {
   return { ...data.user, fullName: `${data.user.firstName} ${data.user.lastName}`, accessToken };
 }
 
+export function getCoverColor(color: "red" | "green" | "blue" | "orange" | "fuchsia") {
+  switch (color) {
+    case "red":
+      return "radial-gradient(circle, #fca5a5, #f87171, #ef4444)";
+    case "green":
+      return "radial-gradient(circle, #86efac, #4ade80, #22c55e)";
+    case "blue":
+      return "radial-gradient(circle, #93c5fd, #60a5fa, #3b82f6)";
+    case "orange":
+      return "radial-gradient(circle, #fdba74, #fb923c, #f97316)";
+    case "fuchsia":
+      return "radial-gradient(circle, #f0abfc, #e879f9, #d946ef)";
+  }
+}
+
+
 export function hasCompleteData(data: Record<string, string>) {
   return Object.values(data).every((value) => value !== "");
 }
