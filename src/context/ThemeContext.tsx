f78788dc5e0df10d9ca46db1cc7ce602d523447c
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-10-10 15:09:38 +0300

    Update ThemeContext to Use Mantine Color Scheme and Default to Dark Theme
    Integrate Mantine's `useMantineColorScheme` hook into the `ThemeContext` to synchronize the theme with Mantine's color scheme. This ensures that the application's theme is consistent across all components that rely on Mantine's theming system. Additionally, change the default theme from "light" to "dark" and update the `toggleTheme` function to also update the Mantine color scheme. This commit also ensures that the theme is persisted in local storage and applied to the document root on initial load.

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,12 +1,15 @@
 import { createContext, useEffect, useState, ReactNode } from "react";
 import { ColorTheme, ThemeContextProps } from "@/types/theme";
+import { useMantineColorScheme } from "@mantine/core";
 
-const initialTheme: ColorTheme = "light";
+const initialTheme: ColorTheme = "dark";
 
 export const ThemeContext = createContext<ThemeContextProps>({ theme: initialTheme, toggleTheme: () => {} });
 
 function ThemeProvider({ children }: { children: ReactNode }) {
-  const [theme, setTheme] = useState<ColorTheme>("light");
+  const [theme, setTheme] = useState<ColorTheme>(initialTheme);
+
+  const { setColorScheme } = useMantineColorScheme();
 
   useEffect(() => {
     let currentTheme = localStorage.getItem("theme");
@@ -15,12 +18,14 @@ function ThemeProvider({ children }: { children: ReactNode }) {
       localStorage.setItem("theme", initialTheme);
     }
     setTheme(currentTheme as ColorTheme);
+    setColorScheme(currentTheme as ColorTheme);
     document.documentElement.classList.add(currentTheme);
   }, []);
 
   const toggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
+    setColorScheme(newTheme);
     document.documentElement.classList.remove(theme);
     document.documentElement.classList.add(newTheme);
     localStorage.setItem("theme", newTheme);
