824cd9bb25960639064ab060e150fcdc1fbd9738
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-10-10 14:56:37 +0300

    Add KebabMenu and Trash icons to outlineIcons and Edit icon to solidIcons
    This commit introduces new icons to the `outlineIcons` and `solidIcons` collections. Specifically, the `KebabMenu` and `Trash` icons have been added to the `outlineIcons` object, and the `Edit` icon has been added to the `solidIcons` object. These additions enhance the icon library, providing more options for UI components.

---
+++ src/components/icons/outline.tsx
@@ -0,0 +1,9 @@
+import { IoTrashOutline } from "react-icons/io5";
+import { CiMenuKebab } from "react-icons/ci";
+
+const outlineIcons = {
+  KebabMenu: CiMenuKebab,
+  Trash: IoTrashOutline,
+};
+
+export default outlineIcons;
--- src/components/icons/solid.tsx
+++ src/components/icons/solid.tsx
@@ -1,9 +1,11 @@
 import { FaPlus } from "react-icons/fa6";
 import { ImSpinner8 } from "react-icons/im";
+import { TbEdit } from "react-icons/tb";
 
 const solidIcons = {
   Plus: FaPlus,
   Spinner: ImSpinner8,
+  Edit: TbEdit,
 };
 
 export default solidIcons;
