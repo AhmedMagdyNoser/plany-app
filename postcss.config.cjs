872009658735ec1d70d275f1664dab245b8cbfd8
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-10-10 13:39:24 +0300

    Update dependencies and configure PostCSS for Mantine UI
    - Added `@mantine/core` and `@mantine/hooks` dependencies for Mantine UI components.
    - Updated `postcss` to version `8.4.47` and added `postcss-preset-mantine` and `postcss-simple-vars` for Mantine-specific PostCSS configurations.
    - Modified `postcss.config.cjs` to include Mantine breakpoint variables and plugins.
    - Integrated `MantineProvider` into the React application to enable Mantine UI theming.
    - Updated `package.json` to reflect the new dependencies and their versions.
    - Updated `src/index.tsx` to wrap the application with `MantineProvider` for Mantine UI support.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,1 +1,16 @@
-module.exports = { plugins: [require("tailwindcss"), require("autoprefixer")] };
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("postcss-preset-mantine"),
+    require("postcss-simple-vars")({
+      variables: {
+        "mantine-breakpoint-xs": "36em",
+        "mantine-breakpoint-sm": "48em",
+        "mantine-breakpoint-md": "62em",
+        "mantine-breakpoint-lg": "75em",
+        "mantine-breakpoint-xl": "88em",
+      },
+    }),
+  ],
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "@mantine/core/styles.css";
 import "./index.css";
 
 // Import context providers
+import { MantineProvider } from "@mantine/core";
 import ThemeProvider from "@/context/ThemeContext";
 import UserProvider from "@/context/UserContext";
 import TasksProvider from "@/context/TasksContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <UserProvider>
-        <TasksProvider>
-          <App />
-        </TasksProvider>
-      </UserProvider>
-    </ThemeProvider>
+    <MantineProvider>
+      <ThemeProvider>
+        <UserProvider>
+          <TasksProvider>
+            <App />
+          </TasksProvider>
+        </UserProvider>
+      </ThemeProvider>
+    </MantineProvider>
   </React.StrictMode>,
 );
