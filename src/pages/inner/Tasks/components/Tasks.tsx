0ce5ef6d0202dc7483caa25c9217e21663da4c26
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-10-11 10:36:48 +0300

    Add sound effect and task completion functionality
    Added a new sound file `completed.mp3` to the assets directory and integrated it into the `Tasks.tsx` component. The sound effect plays when a task is marked as completed. Additionally, implemented the task completion functionality, allowing users to toggle the completion status of a task and update it on the server. The delete functionality was also refactored to use separate loading and error states for better clarity.

--- src/pages/inner/Tasks/components/Tasks.tsx
+++ src/pages/inner/Tasks/components/Tasks.tsx
@@ -5,24 +5,39 @@ import useFetchingStatus from "@/hooks/useFetchingStatus";
 import usePrivateRequest from "@/hooks/usePrivateRequest";
 import useTasks from "@/hooks/useTasks";
 import handleRequest from "@/utils/helpers";
+import completedSound from "@/assets/sounds/completed.mp3";
 
 function Task({ task }: { task: TaskType }) {
   const { setTasks } = useTasks();
 
   const privateRequest = usePrivateRequest();
-  const { loading, setLoading, setError } = useFetchingStatus();
+  const { loading: deleting, setLoading: setDeleting, setError: setDeleteError } = useFetchingStatus();
 
   async function handleDelete() {
-    handleRequest(setLoading, setError, async () => {
+    handleRequest(setDeleting, setDeleteError, async () => {
       await privateRequest({ url: `tasks/${task._id}`, method: "DELETE" });
       setTasks((prevTasks) => prevTasks.filter((t) => t._id !== task._id));
     });
   }
 
+  async function handleComplete() {
+    const updatedTask = await privateRequest({
+      method: "PATCH",
+      url: `tasks/${task._id}`,
+      data: { completed: !task.completed },
+    });
+    setTasks((prevTasks) => prevTasks.map((t) => (t._id === updatedTask._id ? updatedTask : t)));
+    if (!task.completed) {
+      const audio = new Audio(completedSound);
+      audio.volume = 0.35;
+      audio.play();
+    }
+  }
+
   return (
     <div className="rounded-primary bg-basic-3 flex w-full justify-between px-8 py-4 transition-colors">
       <div className="flex items-center gap-4">
-        <button className="flex-center hover:bg-basic-2 rounded-primary h-10 w-10">
+        <button onClick={handleComplete} className="flex-center hover:bg-basic-2 rounded-primary h-10 w-10">
           {task.completed ? (
             <outlineIcons.CheckedCircle size={23.5} className="txt-green" />
           ) : (
@@ -33,7 +48,7 @@ function Task({ task }: { task: TaskType }) {
       </div>
 
       <button onClick={handleDelete} className="hover:txt-red transition-colors" title="Delete this task">
-        {loading ? <solidIcons.Spinner size={16.5} className="animate-spin" /> : <outlineIcons.Trash size={18} />}
+        {deleting ? <solidIcons.Spinner size={16.5} className="animate-spin" /> : <outlineIcons.Trash size={18} />}
       </button>
     </div>
   );
